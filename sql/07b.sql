94586535a0201a12c44572594f25c5d5af9c1e2f
elissa.yz <Elissa.Yanez.25@cmc.edu>
2024-02-29 21:45:22 -0800

    sql7

--- sql/07b.sql
+++ sql/07b.sql
@@ -3,3 +3,15 @@
  * but instead of using the NOT IN operator, you are to use a LEFT JOIN.
  */
 
+SELECT title
+FROM film
+LEFT JOIN inventory USING (film_id)
+LEFT JOIN rental USING (inventory_id)
+LEFT JOIN customer USING (customer_id)
+LEFT JOIN address USING (address_id)
+LEFT JOIN city USING (city_id)
+LEFT JOIN country USING (country_id)
+WHERE country_id = 103 AND film_id IN inventory.film_id
+GROUP BY film.title
+ORDER BY title;
+
